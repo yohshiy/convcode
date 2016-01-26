4df44dc1d9e030da80aeafa92f54ff3934df2d87
yohshiy <yohshiy@gmail.com>
2016-01-26 12:59:31 +0900

    Initial file

---
+++ src/convcode.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+# 
+# Convert charactor codes.
+# 
+
+require 'pp'
+
+$LOAD_PATH << File.dirname(File.expand_path(__FILE__))
+
+
+module ConvCode
+
+  def conv(src, dest, exc_dir_ptn, inc_file_ptn)
+
+  end
+
+end
+
+
+
+
+
+if $0 == __FILE__
+  require 'optparse'
+  
+end
